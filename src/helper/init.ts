42e72f78172bbc8b1a8099754b3e1c5e860654a3
Corefinder89 <soumyajit.basu1988@outlook.com>
2025-02-16 09:50:17 +0530

    Add first commit

---
+++ src/helper/init.ts
@@ -0,0 +1,10 @@
+// const fs = require('fs-extra');
+
+import fs from 'fs-extra';
+
+try {
+  fs.ensureDir('test-results');
+  fs.emptyDir('test-results');
+} catch (error) {
+  console.log('Folder not created! + error');
+}
